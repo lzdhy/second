fd667c13d225b6dc645b62a70489bad9243aa186
giulia <lzdhlly@gmail.com>
2024-01-18 20:47:54 -0300

    projeto

---
+++ desafio.js
@@ -0,0 +1,18 @@
+//classe representando um herói
+class hero{
+    constructor (name, age, kind, power)
+    {
+        this.name = name
+        this.age = age
+        this.kind = kind
+        this.power = power
+    }
+    atacar() {
+        console.log(`${this.kind} atacou usando ${this.power}`)
+        
+    }
+
+}
+//  herí do onmyoji Arena
+const hero1 = new hero("Kise", "30", "monge", "artes maciais")
+hero1.atacar()
